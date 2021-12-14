9702fe6aa0d6f1de646de44a16d0f2f64b15fc58
mengzl <mengzl@bmsk.com>
2021-12-14 09:31:57 +0800

    update

--- src/main/java/com/example/demoquartz/jobs/SchedulerSyncJob.java
+++ src/main/java/com/example/demoquartz/jobs/SchedulerSyncJob.java
@@ -1,12 +1,17 @@
 package com.example.demoquartz.jobs;
 
+import com.example.demoquartz.enitiy.JobInfo;
 import com.example.demoquartz.service.JobInfoService;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
+import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 @Slf4j
 @DisallowConcurrentExecution
@@ -15,14 +20,33 @@ public class SchedulerSyncJob extends QuartzJobBean {
     @Autowired
     private JobInfoService jobInfoService;
 
+    @Autowired
+    private SchedulerFactoryBean schedulerFactoryBean;
+
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
         log.info("SchedulerSyncJob begin... ");
-
-        if (log.isDebugEnabled()) {
-            log.debug(jobInfoService.getCurrentSchedulerJobInfos().toString());
+        List<JobInfo> jobInfos = jobInfoService.getCurrentSchedulerJobInfos();
+        for (JobInfo jobInfo : jobInfos) {
+            JobKey jobKey = new JobKey(jobInfo.getJobName(), jobInfo.getJobGroup());
+            try {
+                if (schedulerFactoryBean.getScheduler().getJobDetail(jobKey) == null) {
+                    addJobAndTrigger(jobInfo);
+                } else {
+                    updateJobAndTrigger(jobInfo);
+                }
+            } catch (SchedulerException e) {
+                log.error(e.getMessage(), e);
+            }
         }
-
         log.info("SchedulerSyncJob end");
+    }
+
+    public void addJobAndTrigger(JobInfo jobInfo) {
+        //TODO add job & trigger
+    }
+
+    public void updateJobAndTrigger(JobInfo jobInfo) {
+        //TODO: update job / trigger
     }
 }
